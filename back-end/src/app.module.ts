f4594a9e3848135c67f35d52b1c7ce3b6541c855
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2025-04-17 21:42:39 -0600

    feat: 🚀 Creación del endpoint para registrar vehículos

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.SUPABASE_DB_URL,    
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
+    VehiclesModule,
+    CommonModule,
+  ],
 })
 export class AppModule {}
---
+++ back-end/src/vehicles/vehicles.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Vehicle } from './entities';
+import { Repository } from 'typeorm';
+import { ExceptionService } from 'src/common/exception.service';
+
+@Injectable()
+export class VehiclesService {
+  constructor(
+    @InjectRepository(Vehicle)
+    private readonly vehicleRepository: Repository<Vehicle>,
+    private readonly exceptionService: ExceptionService
+  ){}
+  
+  async create(createVehicleDto: CreateVehicleDto) {
+    try {
+      const vehicle = this.vehicleRepository.create(createVehicleDto);
+      await this.vehicleRepository.save(vehicle);
+      
+      return vehicle;
+    } catch (error) {
+      this.exceptionService.handleDBExceptions(error)
+    }
+  }
+
+  findAll() {
+    return `This action returns all vehicles`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} vehicle`;
+  }
+
+  update(id: string, updateVehicleDto: UpdateVehicleDto) {
+    return `This action updates a #${id} vehicle`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} vehicle`;
+  }
+}
