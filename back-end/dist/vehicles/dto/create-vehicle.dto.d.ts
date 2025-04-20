97275362d31151c90fc5aea10802f6d7bf03d870
Carlos May <carlosmay220@gmail.com>
2025-04-20 01:04:48 -0600

    fix: 🐛 Agregar carpeta dist

---
+++ back-end/dist/vehicles/dto/create-vehicle.dto.d.ts
@@ -0,0 +1,11 @@
+export declare class CreateVehicleDto {
+    brand: string;
+    model: string;
+    vin: string;
+    licensePlate: string;
+    purchaseDate: Date;
+    cost: number;
+    photo?: string;
+    entryDate: Date;
+    assigned?: boolean;
+}
---
+++ back-end/dist/vehicles/vehicles.service.d.ts
@@ -0,0 +1,15 @@
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { Vehicle } from './entities';
+import { Repository } from 'typeorm';
+import { ExceptionService } from 'src/common/exception.service';
+export declare class VehiclesService {
+    private readonly vehicleRepository;
+    private readonly exceptionService;
+    constructor(vehicleRepository: Repository<Vehicle>, exceptionService: ExceptionService);
+    create(createVehicleDto: CreateVehicleDto): Promise<Vehicle>;
+    findAll(): string;
+    findOne(id: string): Promise<Vehicle>;
+    update(id: string, updateVehicleDto: UpdateVehicleDto): string;
+    remove(id: string): string;
+}
