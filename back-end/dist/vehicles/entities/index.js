93ac48e3baeab31da5a4c30251c5786513fb9d8a
Carlos May <carlosmay220@gmail.com>
2025-04-20 01:06:23 -0600

    fix: 🐛 Actualizar carpeta dist

---
+++ back-end/dist/vehicles/entities/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Vehicle = void 0;
+var vehicle_entity_1 = require("./vehicle.entity");
+Object.defineProperty(exports, "Vehicle", { enumerable: true, get: function () { return vehicle_entity_1.Vehicle; } });
+//# sourceMappingURL=index.js.map
