04fb34dd5d1b83c1a38adc0b555b28e90d5b10cd
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2025-05-28 11:04:45 -0600

    fix: 🐛 Corrección en los mensajes de error 500

--- back-end/src/common/common.module.ts
+++ back-end/src/common/common.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ExceptionService } from './exception.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [ExceptionService],
   exports: [ExceptionService]
 })
