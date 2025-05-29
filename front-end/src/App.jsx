30f260fd159aee5732344110cc7c58bf9cad6c5c
José Carlos Leo Fernández <97575507+JoCaLeFe@users.noreply.github.com>
2025-05-29 13:46:55 -0600

    feat: 🚀 Dashboard con metricas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
-import { SideBar } from "./components/Sidebar";
-import { PrivateRoute } from "./components/PrivateRoute";
+import { Login } from "./auth/Login";
+import { Logout } from "./auth/Logout";
+import { SideBar } from "./components/Sidebar"; 
+import { PrivateRoute } from "./components/PrivateRoute"; 
+import { AssignmentIndex } from "./pages/assignments/AssignmentIndex";
+import { VehicleIndex } from "./pages/vehicles/VehicleIndex";
+import { RouteIndex } from "./pages/routes/RouteIndex";
+import { DriverIndex } from "./pages/drivers/DriverIndex";
+import { EditAssignment } from "./pages/assignments/EditAssignment";
+import { EditVehicle } from "./pages/vehicles/EditVehicles";
+import { EditRoute } from "./pages/routes/EditRoute";
+import { EditDriver } from "./pages/drivers/EditDriver";
+import { Dashboard } from "./dashboard/Dashboard";
 
 import "./index.css";
 
@@ -12,12 +23,30 @@ export const App = () => {
     <div className="min-h-screen bg-gradient-to-br from-purple-900 to-gray-900 text-white">
       <BrowserRouter>
         <Routes>
-          {/* Public routes */}
+          <Route path="/login" element={<Login onLogin={setUser} />} /> 
+          {/* <Route path="/register" element={<Register />} />  */}
+          <Route path="/logout" element={<Logout />} /> 
 
-          {/* Protected routes */}
           {user ? (
             <Route element={<PrivateRoute />}>
               <Route element={<SideBar />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+
+                <Route path="/assignment" element={<AssignmentIndex />} />
+                <Route path="/assignment/new" element={<EditAssignment />} /> 
+                <Route path="/assignment/:id/edit" element={<EditAssignment />} /> 
+                
+                <Route path="/vehicle" element={<VehicleIndex />} /> 
+                <Route path="/vehicle/new" element={<EditVehicle />} /> 
+                <Route path="/vehicle/:id/edit" element={<EditVehicle />} /> 
+
+                <Route path="/driver" element={<DriverIndex />} />
+                <Route path="/driver/new" element={<EditDriver />} /> 
+                <Route path="/driver/:id/edit" element={<EditDriver />} /> 
+
+                <Route path="/route" element={<RouteIndex />} /> 
+                <Route path="/route/new" element={<EditRoute />} /> 
+                <Route path="/route/:id/edit" element={<EditRoute />} /> 
               </Route>
             </Route>
           ) : (
